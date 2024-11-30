bcb0d25a4166db9e02081d2d62567d8c6901bf44
Yohannes Teshome <katteyohannes@gmail.com>
2024-11-30 13:41:49 -0500

    remove old file and replace by new one

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-import createNextIntlPlugin from "next-intl/plugin";
-
-const withNextIntel = createNextIntlPlugin()
-
-/** @type { import('next').NextConfig} **/ 
-const nextConfig = {}
-
-export default withNextIntel(nextConfig)
--- src/app/[locale]/not-found.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-export default function NotFound(){
-    return(
-        <h1>Not Found</h1>
-    )
-}
