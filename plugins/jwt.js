0a9cfecd493da3d56c5bb47d21c21588b7ef8ed8
Yohannes Teshome <kateyohannes@gmail.com>
2024-12-28 11:27:09 -0500

    Initial commit (via bun create)

--- plugins/jwt.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const fp = require("fastify-plugin")
-
-module.exports = fp(async (fastify, options)=>{
-    fastify.register(require("@fastify/jwt"),{
-        secret : fastify.config.JWT_SECRET
-    });    
-})
