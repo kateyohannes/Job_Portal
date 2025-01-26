022dc5037d6f06bd6748e337daf2647554afba6b
Yohannes Teshome <kateyohannes@gmail.com>
2025-01-26 07:39:20 -0500

    modife the server base on the requirement

--- src/server.js
+++ src/server.js
@@ -11,6 +11,32 @@ const fastify = require('fastify')({
 // });
 
 // fastify.register(require('@fastify/etag'))
+
+const __start__ = async ()=>{
+    try{
+        await fastify.register(require("@fastify/autoload"),{
+            dir: path.join(__dirname, 'plugins')
+        });
+
+        await fastify.register(require('./routes'), {
+            prefix: '/api/v1',
+        });
+
+        await fastify.listen({ 
+            port: 4000, 
+            host: '0.0.0.0' 
+        });
+    }catch(err){
+        fastify.log.error(err)
+        process.exit(1)
+    }
+}
+
+__start__()
+
+
+
+
 // (async()=>{
 //     try{
 //         await fastify.register(require("@fastify/autoload"),{
@@ -28,25 +54,3 @@ const fastify = require('fastify')({
 //         process.exit(1)
 //     }
 // })();
-
-const __start__ = async ()=>{
-    try{
-        await fastify.register(require("@fastify/autoload"),{
-            dir: path.join(__dirname, 'plugins')
-        })
-        await fastify.register(require('./routes'), {
-            prefix: '/api/v1',
-        })
-        await fastify.listen({ 
-            port: 4000, 
-            host: '0.0.0.0' 
-        })
-    }catch(err){
-        fastify.log.error(err)
-        process.exit(1)
-    }
-}
-
-__start__()
-
-
